ae40f504996ce106f140dac516ef53d44455d5b6
Nguyen Ngoc Huy <huynguyen1305@nexondv.com>
2023-01-09 14:47:37 +0700

    test: fix favicon 5

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -48,7 +48,7 @@ function App({ i18n }) {
   return (
     <AppThemeProvider>
       <NotificationsProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/live-api-openapi-frontweb">
           <Helmet
             titleTemplate="%s - Nexon Developer Center"
             defaultTitle="Nexon Developer Center"
