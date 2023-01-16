0b708dcad976a6843b6ff2fb21c7c9e2c4cfe34e
Nguyen Pham Quang Dinh <destnguyxn@nexondv.com>
2023-01-16 13:34:33 +0700

    fix: not load assets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -48,7 +48,7 @@ function App({ i18n }) {
   return (
     <AppThemeProvider>
       <NotificationsProvider>
-        <BrowserRouter basename="/frontweb-openapi">
+        <BrowserRouter basename="/live-api-openapi-frontweb">
           <Helmet
             titleTemplate="%s - Nexon Developer Center"
             defaultTitle="Nexon Developer Center"
