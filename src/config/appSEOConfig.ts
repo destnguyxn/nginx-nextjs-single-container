c36f7d086a192049214f3d2d5adbb3206cd1814a
Nguyen Ngoc Huy <huynguyen1305@nexondv.com>
2023-02-01 17:11:47 +0700

    Revert to 56e05fced

--- src/config/appSEOConfig.ts
+++
@@ -1,9 +0,0 @@
-// FIXME: Update this configuration file based on your project information
-
-export const appSEOConfig = {
-  site_name: 'NDVN Starter',
-  title: 'NDVN Starter',
-  description: 'NDVN Starter code with Nextjs + Mantine + Tailwind CSS',
-  locale: 'en',
-  canonical: 'http://localhost:3000',
-};
--- src/constants/type.ts
+++
@@ -1,6 +0,0 @@
-export interface Pokemons {
-  id: string;
-  name: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
