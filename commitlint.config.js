e554ef6d7a5a7795dacfceba1c92a2da6cf4cace
Nguyen Ngoc Huy <huynguyen1305@nexondv.com>
2023-02-03 08:59:08 +0700

    feat: NDS-86-homeHeader

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,19 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
+    'subject-case': [
       2,
       'always',
-      ['feat', 'fix', 'refactor', 'test', 'build', 'ci', 'revert'],
-    ],
-
-    'subject-case': [
-      'lower-case',
-      'upper-case',
-      'camel-case',
-      'kebab-case',
-      'pascal-case',
-      'sentence-case',
-      'snake-case',
-      'start-case',
+      [
+        'lower-case',
+        'upper-case',
+        'camel-case',
+        'kebab-case',
+        'pascal-case',
+        'sentence-case',
+        'snake-case',
+        'start-case',
+      ],
     ],
   },
 };
