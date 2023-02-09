5872c1fca63911fe6b23487928d34f4df288c456
Nguyen Pham Quang Dinh <destnguyxn@nexondv.com>
2023-02-09 16:22:45 +0700

    build: fix warning in nextjs unsupport config and supervisor user root

--- next.config.js
+++ next.config.js
@@ -8,15 +8,16 @@ let config = {
     dirs: ['.'],
   },
   poweredByHeader: false,
-  trailingSlash: false,
-  basePath: '',
+  trailingSlash: true,
+  basePath: '/live-api-openapi-frontweb',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
   i18n,
   output: 'standalone',
-  distDir: 'out',
+  distDir: '_next',
+  assetPrefix: isNotProduction ? undefined : '/live-api-openapi-frontweb',
   images: {
     remotePatterns: [
       {
