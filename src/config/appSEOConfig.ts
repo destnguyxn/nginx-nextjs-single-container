dd8a0977eee1c8fb1ba8de014363b040c70384f3
erudilumos <chromevi123@gmail.com>
2023-02-15 21:06:06 +0700

    feat: remove not necessary code

--- src/config/appSEOConfig.ts
+++ src/config/appSEOConfig.ts
@@ -1,9 +1,9 @@
 // FIXME: Update this configuration file based on your project information
 
 export const appSEOConfig = {
-  site_name: 'Nexon Open Api',
-  title: 'Nexon Open Api',
-  description: `Based on Nexon's numerous data. Make your own service your own color`,
+  site_name: 'Next JS',
+  title: 'Next Simple',
+  description: `your own`,
   locale: 'en',
   canonical: 'http://localhost:3000',
 };
--- src/modules/pokemons/services.ts
+++
@@ -1,6 +0,0 @@
-import axiosClient from '@/config/axiosClient';
-
-export const getPokemons = async () => {
-  const { data } = await axiosClient.get('/photos?_start=0&_limit=20');
-  return data;
-};
